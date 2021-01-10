8d93cd73a9cac3c6326aeeb9e6c878b2af301a62
Steffan Carrington <steffan.carrington@inviqa.com>
2021-01-10 17:38:12 +0000

    Downloaded dev dependencies to get Jest up and running on Pokedex project. Implemented first test.

---
+++ __tests__/displayPokemon.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import { render } from '@testing-library/jest-dom';
+import {render} from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import PokemonInfo from '../components/PokemonInfo/PokemonInfo';
+
+describe('Pokemon Info', () => {
+    let expectedProps;
+
+    beforeEach(() => {
+        expectedProps = {
+            name: 'Bulbasaur',
+            image: 'https://placehold.it',
+            description: 'Example Description here',
+            height: '134',
+            weight: '23',
+            stats: [{}]
+        }
+    });
+
+    test('Should render Pokemon Name, ID, Image, Description, Height, Weight and Stats', () => {
+        const { getByText, getByAltText } = render(<PokemonInfo {...expectedProps} />);
+        const name = getByText(expectedProps.name);
+        const image = getByAltText(expectedProps.image);
+        const description = getByAltText(expectedProps.description);
+
+        expect(name).toBeVisible();
+        expect(id).toBeVisible();
+        expect(image).toBeVisible();
+        expect(description).toBeVisible();
+    });
+});
